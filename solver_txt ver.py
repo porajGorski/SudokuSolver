733770f09f642d948f9eef9efc123048dc74603a
porajGorski <poraj.gorski@gmail.com>
2020-12-30 15:49:42 +0100

    corrections

--- solver_txt ver.py
+++ solver_txt ver.py
@@ -82,7 +82,7 @@ def print_board(board):
     for i in range(len(board)):
         if i%3 == 0 and i != 0:
             print("- - - - - - - - - - - - - -")
-        for j in range(len(bo[0])):
+        for j in range(len(board[0])):
             if j%3 == 0:
                 print(" | ", end="")
             if j == 8:
